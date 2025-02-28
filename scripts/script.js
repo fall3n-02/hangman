e9415019003c66105008e030941661713c7b0fe2
Peter Biryukov <biryukovpa26@gmail.com>
2025-02-28 18:02:41 +0300

    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,1 @@
-const greetingsDialog = document.querySelector("#greetings-dialog");
-const startGameButton = document.querySelector("#form-button");
-
-window.onload = function () {
-  greetingsDialog.showModal();
-}
-
-startGameButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  greetingsDialog.close();
-  console.log("FINE");
-  let inputWord = document.querySelector("#input-word").value;
-  let inputDescription = document.querySelector("#input-description").value;
-})
+const 
